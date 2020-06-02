1f401c7a031b0bf12bb1a09dba08b0df0f9decab
shoruk <shorouk@keyplanz.com.eg>
2020-06-02 10:13:55 +0200

    add features section with owl-carousel

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:12,
+    nav: false,
+    navContainer:false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:3,
+            margin:16
+        }
+    }
+})
